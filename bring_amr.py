ae626b8b29b30a3b43eb3b2f02fcf454b7dfbef7
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2022-05-18 13:08:36 +0700

    Select navigation and fixposition

--- bring_amr.py
+++ bring_amr.py
@@ -1,20 +1,21 @@
 import os
 import time 
 
-# os.system("roslaunch robotamr Keep_position.launch")
-# time.sleep(2)
-
-''' Check Path  '''
 # print(os.getcwd())  
 
 def check_path_go (path):
-  if os.path.isdir(path):
-    os.chdir(path)
-    return f"now:{os.getcwd()}"
-  else:
-    print("Fail to go path.")
-    return False 
+    if os.path.isdir(path):
+        os.chdir(path)
+        return f"now:{os.getcwd()}"
+    else:
+        print("Fail to go path.")
+        return False 
 
-path_script = "/home/niboon/egat_rb/src/robotamr/Script/GUI_Terminal_egat/"
-check_path_go(path_script)
-os.system("python Send_goal.py")
+user = input("Select (1 . Navigation ) and (2 . Fix position ) : ")
+
+if user == '1':
+    os.system("roslaunch robotamr Keep_position.launch")
+elif (user == '2'):
+    path_script = "/home/niboon/egat_rb/src/robotamr/Script/GUI_Terminal_egat/"
+    check_path_go(path_script)
+    os.system("python Send_goal.py")
