4e54d9d95c88f43ea84a311897356a8a462e1339
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2022-07-17 14:27:47 +1000

    Update bring_amr.py

--- bring_amr.py
+++ bring_amr.py
@@ -10,12 +10,15 @@ def check_path_go (path):
     else:
         print("Fail to go path.")
         return False 
+        
+path_script = "/home/niboon/egat_rb/src/robotamr/Script/GUI_Terminal_egat/"
+check_path_go(path_script)
 
+print("\t\t\t  Welcome to EGAT-AMR \n ")
 user = input("Select (1 . Navigation ) and (2 . Fix position ) : ")
 
 if user == '1':
     os.system("roslaunch robotamr Keep_position.launch")
 elif (user == '2'):
-    path_script = "/home/niboon/egat_rb/src/robotamr/Script/GUI_Terminal_egat/"
-    check_path_go(path_script)
+    
     os.system("python Send_goal.py")
